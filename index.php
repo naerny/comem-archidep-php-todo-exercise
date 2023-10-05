ff036f4744c1d3baf1c6c0165b56cb1f2cb60ac5
Tristan Montanari <tristan.montanari@heig-vd.ch>
2023-10-05 11:36:48 +0200

    Implement functionality to toggle a task

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ if (isset($_POST['action'])) {
 
       $id = $_POST['id'];
       if(is_numeric($id)) {
-        $updateQuery = ''; // IMPLEMENT ME
+        $updateQuery = 'UPDATE todo SET done = NOT done WHERE id = ' . $id; // IMPLEMENT ME (Tristan)
         if(!$db->query($updateQuery)) {
           die(print_r($db->errorInfo(), true));
         }
@@ -60,7 +60,7 @@ if (isset($_POST['action'])) {
 
       $id = $_POST['id'];
       if(is_numeric($id)) {
-        $deleteQuery = 'DELETE FROM todo WHERE id =' . $id; // IMPLEMENT MEs
+        $deleteQuery = 'DELETE FROM todo WHERE id =' . $id; // IMPLEMENT ME (Loic)
         if(!$db->query($deleteQuery)) {
           die(print_r($db->errorInfo(), true));
         }
@@ -77,7 +77,7 @@ if (isset($_POST['action'])) {
 /**
  * Select all tasks from the database.
  */
-$selectQuery = 'SELECT * FROM `todo` '; // IMPLEMENT ME
+$selectQuery = 'SELECT * FROM `todo` '; // IMPLEMENT ME (Nicolas)
 $items = $db->query($selectQuery);
 ?>
 
